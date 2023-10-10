5bf2cb727ce4b744c135074c2282387f58f341a2
AndreHernandezJ <144179386+AndreHernandezJ@users.noreply.github.com>
2023-10-10 15:25:08 -0600

    Add files via upload

---
+++ p2/p2p3_1s21.java
@@ -0,0 +1,10 @@
+public class p2p3_1s21{
+	public static void main(String[] args) {
+		int a, b;
+		for(a=0;a<11;a++){
+			for(b=1;b<11;b++){
+				System.out.println(a+"x"+b+"="+(a*b));
+			}
+		}
+	}
+}
---
+++ p2/p2p4_1s21.java
@@ -0,0 +1,11 @@
+public class p2p4_1s21{
+	public static void main(String[] args) {
+		int max=10;
+		for(int x=2;x<max;x++){
+			for(int i=2;i<x;i++){
+				break;
+			System.out.println(x+" es un numero primo");
+			}
+		}
+	}
+}
