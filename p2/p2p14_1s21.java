71d595a25a15c9eb6f3f1f282e468d681e123dfa
AndreHernandezJ <144179386+AndreHernandezJ@users.noreply.github.com>
2023-10-24 16:08:42 -0600

    Add files via upload

---
+++ p2/p2p14_1s21.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class p2p14_1s21{
+	public static void main(String[] args) {
+	Scanner teclado=new Scanner(System.in);
+	int[][] Ma, Mb, Mc;
+	int fa,ca,fb,cb,f,c, r;
+	System.out.println("Ingresa el numero de filas para Matriz A");
+	fa = teclado.nextInt();
+	System.out.println("Ingresa el numero de columnas para la matriz A");
+	ca = teclado.nextInt();
+	System.out.println("Ingresa el numero de filas para Matriz B");
+	fb = teclado.nextInt();
+	System.out.println("Ingresa el numero de columnas para la matriz B");
+	cb = teclado.nextInt();
+	if (ca==fb){
+		Ma= int [fa][ca];
+		Mb= int [fb][cb];
+		Mc= int [fa][cb];
+		for (f=0;f<fa;f++){
+			for(c=0;c<ca;c++){
+				System.out.println("Ingresa el valor para Ma["+f+"]["+c+"]: ");
+				Ma[f][c]=teclado.nextInt();
+			}
+		}
+		for (f=0;f<fb;f++){
+			for(c=0;c<cb;c++){
+				System.out.println("Ingresa el valor para Ma["+f+"]["+c+"]: ");
+				Mb[f][c]=teclado.nextInt();
+			}
+		}
+		//aqui iniciamos la multiplicacion
+		for (f=0;f<fa;f++){
+			for(c=0;c<cb;c++){
+				for(h=0;h<fb;h++){
+					r=Ma[f][h]*Mb[c][h];
+			}
+		}
+	}
+	}else{
+		System.out.println("No se puede realizar el proceso de la multiplicacion");
+		}
+	}
+}
